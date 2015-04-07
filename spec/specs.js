c5d24f11a760255ff5c20e2e18bfd136155960c1
Conor Baumgart & Brian Kropff <student@epicodus.com>
2015-04-07 10:07:34 -0700

    Add test for taking a mix of uppercase and lowercase letters and turn them into title case

--- spec/specs.js
+++ spec/specs.js
@@ -6,5 +6,8 @@ describe("titleCase", function() {
     it("makes all letters of the word lowercase, except for the first", function() {
         expect(titleCase1("CAT")).to.equal("Cat");
     });
-    
+
+    it("takes a mix of uppercase and lowercase letters and return the word in titlecase", function() {
+        expect(titleCase1("KittyCat")).to.equal("Kittycat");
+    })
 });
