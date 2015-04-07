fd3c9ca4c0547b772f374f9daab6f569b0ca9d7e
Conor Baumgart & Brian Kropff <student@epicodus.com>
2015-04-07 10:06:13 -0700

    Add function and test to make an all uppercase word into titlecase

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 function titleCase1(word) {
     // var new_word = word[0].toUpperCase() + word.slice(1);
     word = word.replace(word[0], word[0].toUpperCase());
+    var i = 1;
+    var j = 2;
+    while(i<(word.length)) {
+        word = word.replace(word[i], word[i].toLowerCase());
+        i++;
+    }
     return word;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,9 @@ describe("titleCase", function() {
     it("capitalizes the first letter of the word", function() {
         expect(titleCase1("cat")).to.equal("Cat");
     });
+
+    it("makes all letters of the word lowercase, except for the first", function() {
+        expect(titleCase1("CAT")).to.equal("Cat");
+    });
+    
 });
