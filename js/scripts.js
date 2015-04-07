560276c18ed2b20fe19b6dc0a73b9dc9f2806b59
Conor Baumgart & Brian Kropff <student@epicodus.com>
2015-04-07 11:25:13 -0700

    Adds html page

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,30 @@
 function titleCase1(word_entered) {
-    // var new_word = word[0].toUpperCase() + word.slice(1);
-    var array = word_entered.split(" ");
-    for (var j = 0, len = array.length; j < len; j++) {
-        var word = array[j];
-        word = word.replace(word[0], word[0].toUpperCase());
-        var i = 1;
-        while(i<(word.length)) {
-            word = word.replace(word[i], word[i].toLowerCase());
-            i++;
-        }
-        array[j] = word;
+  // var new_word = word[0].toUpperCase() + word.slice(1);
+  var array = word_entered.split(" ");
+  for (var j = 0, len = array.length; j < len; j++) {
+    var word = array[j];
+    word = word.replace(word[0], word[0].toUpperCase());
+    var i = 1;
+    while(i<(word.length)) {
+        word = word.replace(word[i], word[i].toLowerCase());
+        i++;
     }
-    var results = array.join(" ");
-    return results;
+    if ((word === "And") || (word === "Or") || (word ==="The") && (j !== 0)) {
+      word = word.replace(word[0], word[0].toLowerCase());
+    }
+    array[j] = word;
+  }
+
+  var results = array.join(" ");
+  return results;
 }
+
+$(document).ready(function() {
+  $("form#translator").submit(function(event) {
+    var word = $("input").val();
+    var result = titleCase1(word);
+    $(".returned_word").text(result);
+    $("#result").show();
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,21 @@
 describe("titleCase", function() {
-    it("capitalizes the first letter of the word", function() {
-        expect(titleCase1("cat")).to.equal("Cat");
-    });
+  it("capitalizes the first letter of the word", function() {
+      expect(titleCase1("cat")).to.equal("Cat");
+  });
 
-    it("makes all letters of the word lowercase, except for the first", function() {
-        expect(titleCase1("CAT")).to.equal("Cat");
-    });
+  it("makes all letters of the word lowercase, except for the first", function() {
+      expect(titleCase1("CAT")).to.equal("Cat");
+  });
 
-    it("takes a mix of uppercase and lowercase letters and return the word in titlecase", function() {
-        expect(titleCase1("KittyCat")).to.equal("Kittycat");
-    })
+  it("takes a mix of uppercase and lowercase letters and return the word in titlecase", function() {
+      expect(titleCase1("KittyCat")).to.equal("Kittycat");
+  });
 
-    it("takes multiple words and returns each into titlecase", function() {
-        expect(titleCase1("Meat bALL1")).to.equal("Meat Ball1");
-    })
+  it("takes multiple words and returns each into titlecase", function() {
+      expect(titleCase1("Meat bALL1")).to.equal("Meat Ball1");
+  });
+
+  it("will treat specific words as all lower case", function() {
+      expect(titleCase1("The Meat OR BAll AnD 2")).to.equal("The Meat or Ball and 2");
+  });
 });
